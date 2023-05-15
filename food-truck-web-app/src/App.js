12b7670c700a3b97051d284ff76e6a4434c600fd
shankervalipireddy <shanker.valipireddy@gmail.com>
2023-05-14 18:58:50 -0700

    Update App.js
    made changes

--- food-truck-web-app/src/App.js
+++ food-truck-web-app/src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Table, Pagination  ,Form, Button} from "react-bootstrap";
-
+import { Table, Pagination, Form, Button } from "react-bootstrap";
 
 function App() {
   const [foodTrucks, setFoodTrucks] = useState([]);
@@ -28,75 +27,80 @@ function App() {
   // Change page
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
- // Function to handle nearby food trucks endpoint
+  // Function to handle nearby food trucks endpoint
   const getNearbyFoodTrucks = () => {
-   //TODO
+    // TODO
   };
 
   // Function to handle posting on social media endpoint
   const postOnSocialMedia = (post) => {
-   //TODO
+    // TODO
   };
 
   // Function to handle registering for loyalty program endpoint
   const registerForLoyaltyProgram = (customer) => {
-  //TODO
+    // TODO
   };
 
   // Function to handle submitting a review endpoint
   const submitReview = (review) => {
-   //TODO
+    // TODO
   };
 
   // Function to handle responding to a review endpoint
   const respondToReview = (reviewId, response) => {
-   //TODO
-    };
+    // TODO
+  };
+
   return (
-    <div>
-      <h1>Food Truck Web App</h1>
-      <h2>Food Truck List in San Fransisco:</h2>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Cuisine</th>
-            <th>Location</th>
-          </tr>
-        </thead>
-        <tbody>
-          {currentTrucks.map((foodTruck) => (
-            <tr key={foodTruck.locationid}>
-              <td>{foodTruck.applicant}</td>
-              <td>{foodTruck.foodItems}</td>
-              <td>{foodTruck.locationDescription}</td>
+    <div className="container">
+      <h1 className="text-center mt-5">Food Truck Web App</h1>
+      <div className="my-5">
+        <h2 className="text-center">Food Truck List in San Francisco:</h2>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Cuisine</th>
+              <th>Location</th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
-      <Pagination>
-        <Pagination.Prev
-          onClick={() => paginate(currentPage - 1)}
-          disabled={currentPage === 1}
-        />
-        {Array.from({ length: Math.ceil(foodTrucks.length / trucksPerPage) }).map(
-          (_, index) => (
-            <Pagination.Item
-              key={index}
-              active={index + 1 === currentPage}
-              onClick={() => paginate(index + 1)}
-            >
-              {index + 1}
-            </Pagination.Item>
-          )
-        )}
-        <Pagination.Next
-          onClick={() => paginate(currentPage + 1)}
-          disabled={currentPage === Math.ceil(foodTrucks.length / trucksPerPage)}
-        />
-      </Pagination>
-      
-       <Form onSubmit={getNearbyFoodTrucks}>
+          </thead>
+          <tbody>
+            {currentTrucks.map((foodTruck) => (
+              <tr key={foodTruck.locationid}>
+                <td>{foodTruck.applicant}</td>
+                <td>{foodTruck.foodItems}</td>
+                <td>{foodTruck.locationDescription}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+        <Pagination className="justify-content-center">
+          <Pagination.Prev
+            onClick={() => paginate(currentPage - 1)}
+            disabled={currentPage === 1}
+          />
+          {Array.from({ length: Math.ceil(foodTrucks.length / trucksPerPage) }).map(
+            (_, index) => (
+              <Pagination.Item
+                key={index}
+                active={index + 1 === currentPage}
+                onClick={() => paginate(index + 1)}
+              >
+                {index + 1}
+              </Pagination.Item>
+            )
+          )}
+          <Pagination.Next
+            onClick={() => paginate(currentPage + 1)}
+            disabled={currentPage === Math.ceil(foodTrucks.length / trucksPerPage)}
+          />
+        </Pagination>
+      </div>
+
+      <div className="my-5">
+        <h2 className="text-center">Get Nearby Food Trucks</h2>
+                <Form onSubmit={getNearbyFoodTrucks} className="mb-5">
           <Form.Group>
             <Form.Label>Latitude</Form.Label>
             <Form.Control type="text" />
@@ -113,7 +117,9 @@ function App() {
             Get Nearby Food Trucks
           </Button>
         </Form>
-        <Form onSubmit={postOnSocialMedia}>
+
+        <h2 className="text-center">Post on Social Media</h2>
+        <Form onSubmit={postOnSocialMedia} className="mb-5">
           <Form.Group>
             <Form.Label>Content</Form.Label>
             <Form.Control as="textarea" rows={3} />
@@ -122,7 +128,9 @@ function App() {
             Post on Social Media
           </Button>
         </Form>
-        <Form onSubmit={registerForLoyaltyProgram}>
+
+        <h2 className="text-center">Register for Loyalty Program</h2>
+        <Form onSubmit={registerForLoyaltyProgram} className="mb-5">
           <Form.Group>
             <Form.Label>Name</Form.Label>
             <Form.Control type="text" />
@@ -135,6 +143,8 @@ function App() {
             Register for Loyalty Program
           </Button>
         </Form>
+
+        <h2 className="text-center">Submit a Review</h2>
         <Form onSubmit={submitReview}>
           <Form.Group>
             <Form.Label>Food Truck ID</Form.Label>
@@ -152,11 +162,10 @@ function App() {
             Submit Review
           </Button>
         </Form>
-       
+      </div>
     </div>
-    
-    
   );
 }
 
 export default App;
+
